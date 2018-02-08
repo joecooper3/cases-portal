5cb6ebd343c9bea4c7164a8f64cbe3556affc05b
Joe Cooper <joecooper3@gmail.com>
2018-02-08 16:52:24 -0500

    Refractors so AJAX request is made in department.jsx

--- src/components/DeptFetch.jsx
+++ src/components/DeptFetch.jsx
@@ -1,63 +1,17 @@
 import React from 'react';
 import {StaffBox} from './StaffBox.jsx';
 
-const data = 'http://localhost:8888/cases-portal/wp-content/themes/cases_portal/data/convertcsv.json';
-
 class DeptFetch extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      parts: [],
-      supervisorParts: []
-    };
-  }
-  componentWillMount() {
-    let titleBlock = document.getElementById('dept-title');
-    let dept = titleBlock.getAttribute('data-id');
-    let supervisor = titleBlock.getAttribute('supervisor-id');
-    fetch(data).then((response) => {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' + response.status);
-        return;
-      }
-      response.json().then((data) => {
-        let filteredArray = [];
-        let supervisorArray = [];
-        data.info.map((info) => {
-          if (supervisor.includes(info.email)) {
-          supervisorArray.push(info);
-          }
-          else if(info.department === dept && info.program == "") {
-          filteredArray.push(info);
-        }
-      })
-      function compare(a,b) {
-        let nameA = a.last.toUpperCase();
-        let nameB = b.last.toUpperCase();
-          if (nameA < nameB)
-            return -1;
-          if (nameA > nameB )
-            return 1;
-          return 0;
-        }
-      let sortedArray = filteredArray.sort(compare);
-      this.setState({parts: sortedArray});
-      this.setState({supervisorParts: supervisorArray});
-      });
-    }).catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
-  }
   render() {
     return (
       <div id="dept-contacts">
         <div className="top-supervisor">
-          {this.state.supervisorParts.map((part, i) =>
+          {this.props.supervisorParts.map((part, i) =>
               <StaffBox key={i} first={part.first} last={part.last} title={part.title}
                 email={part.email} phone={part.phone} supervisor={part.supervisor}/>
           )}
         </div>
-      {this.state.parts.map((part, i) =>
+      {this.props.parts.map((part, i) =>
         <StaffBox key={i} first={part.first} last={part.last} title={part.title}
           email={part.email} phone={part.phone} supervisor={part.supervisor} />
     )}
--- src/department.jsx
+++ src/department.jsx
@@ -6,13 +6,60 @@ import {render} from 'react-dom';
 import {DeptFetch} from './components/DeptFetch.jsx';
 import {ProgramsUnitsList} from './components/ProgramsUnitsList.jsx';
 
-const data = 'http://localhost:8888/cases-portal/wp-content/themes/cases_portal/data/dummy.json';
+const data = 'http://localhost:8888/cases-portal/wp-content/themes/cases_portal/data/convertcsv.json';
+const staffUrl = 'http://localhost:8888/cases-portal/wp-json/wp/v2/staff?_embed=true&per_page=50';
+const requestUrls = [data, staffUrl];
 
 class DeptFetchApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      parts: [],
+      supervisorParts: []
+    };
+  }
+  componentWillMount() {
+    let titleBlock = document.getElementById('dept-title');
+    let dept = titleBlock.getAttribute('data-id');
+    let supervisor = titleBlock.getAttribute('supervisor-id');
+    fetch(data).then((response) => {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
+        return;
+      }
+      response.json().then((data) => {
+        let filteredArray = [];
+        let supervisorArray = [];
+        data.info.map((info) => {
+          if (supervisor.includes(info.email)) {
+          supervisorArray.push(info);
+          }
+          else if(info.department === dept && info.program == "") {
+          filteredArray.push(info);
+        }
+      })
+      function compare(a,b) {
+        let nameA = a.last.toUpperCase();
+        let nameB = b.last.toUpperCase();
+          if (nameA < nameB)
+            return -1;
+          if (nameA > nameB )
+            return 1;
+          return 0;
+        }
+      let sortedArray = filteredArray.sort(compare);
+      this.setState({parts: sortedArray});
+      this.setState({supervisorParts: supervisorArray});
+      });
+    }).catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+  }
+
   render () {
     return (
       <div>
-      <DeptFetch />
+      <DeptFetch supervisorParts={this.state.supervisorParts} parts={this.state.parts} />
     </div>
     );
   }
