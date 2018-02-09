39e56d99658a3892ea690a9d995f658f6149c365
Joe Cooper <joecooper3@gmail.com>
2018-02-09 18:23:05 -0500

    Refractored staff pages

--- src/components/StaffFetch.jsx
+++ src/components/StaffFetch.jsx
@@ -4,39 +4,14 @@ import {StaffBox} from './StaffBox.jsx';
 const data = 'http://localhost:8888/cases-portal/wp-content/themes/cases_portal/data/convertcsv.json';
 
 class StaffFetch extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      parts: [],
-    };
-  }
-  componentWillMount() {
-    let titleBlock = document.getElementById('staff-title');
-    let email = titleBlock.getAttribute('staff-email');
-    fetch(data).then((response) => {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' + response.status);
-        return;
-      }
-      response.json().then((data) => {
-        let filteredArray = [];
-        data.info.map((info) => {
-          if(info.email === email) {
-          filteredArray.push(info);
-        }
-      })
-      this.setState({parts: filteredArray});
-      });
-    }).catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
-  }
   render() {
     return (
       <div id="single-contact">
-      {this.state.parts.map((part, i) =>
+      {this.props.parts.map((part, i) =>
         <StaffBox key={i} first={part.first} last={part.last} title={part.title}
-          email={part.email} phone={part.phone} supervisor={part.supervisor} />
+          email={part.email} phone={part.phone} supervisor={part.supervisor}
+          imageUrl={part.imageUrl} url={part.url} supervisorUrl={part.supervisorUrl}
+          supervisorName={part.supervisorName} />
     )}
       </div>
 
--- src/staff.jsx
+++ src/staff.jsx
@@ -6,7 +6,16 @@ import {render} from 'react-dom';
 import {StaffBreadcrumbs} from './components/StaffBreadcrumbs.jsx';
 import {StaffFetch} from './components/StaffFetch.jsx';
 
-const data = 'http://localhost:8888/cases-portal/wp-content/themes/cases_portal/data/dummy.json';
+const data = 'http://localhost:8888/cases-portal/wp-content/themes/cases_portal/data/convertcsv.json';
+const staffUrl = 'http://localhost:8888/cases-portal/wp-json/wp/v2/staff?_embed=true&per_page=50';
+const requestUrls = [data, staffUrl];
+
+const apiRequest1 = fetch(data).then(function(response) {
+  return response.json()
+});
+const apiRequest2 = fetch(staffUrl).then(function(response) {
+  return response.json()
+});
 
 class StaffBreadcrumbsApp extends React.Component {
   render () {
@@ -17,10 +26,58 @@ class StaffBreadcrumbsApp extends React.Component {
 }
 
 class StaffFetchApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      parts: [],
+      supervisorParts: [],
+      staffAPI: [],
+    };
+  }
+  componentWillMount() {
+    let titleBlock = document.getElementById('staff-title');
+    let email = titleBlock.getAttribute('staff-email');
+    Promise.all([apiRequest1,apiRequest2]).then(values => {
+      let filteredArray = [];
+      let jasonData = values[0].info;
+      let staffPages = values[1];
+      values[0].info.map((info) => {
+        if(info.email === email) {
+        filteredArray.push(info);
+      }
+    })
+    function supervisorUrlPull(soup){
+      let soupEmail = soup + "@cases.org";
+      for (let i = 0; i < staffPages.length; i++) {
+        if (staffPages[i].acf.email == soupEmail) {
+          return staffPages[i].link;
+        }
+      };
+    }
+    function supervisorNamePull(soup){
+      let soupEmail = soup + "@cases.org";
+      for (let i = 0; i < jasonData.length; i++) {
+        if (jasonData[i].email == soupEmail) {
+          return jasonData[i].first + " " + jasonData[i].last;
+        }
+      };
+    }
+    filteredArray.forEach(function(item) {
+      var result = staffPages.filter(function(staffItem) {
+          return staffItem.acf.email === item.email;
+      });
+    item.url = (result[0] !== undefined) ? result[0].link : null;
+    item.imageUrl = (result[0] !== undefined) ? result[0]._embedded['wp:featuredmedia'][0]['source_url'] : null;
+    item.supervisorUrl = (result[0] !== undefined) ? supervisorUrlPull(item.supervisor) : null;
+    item.supervisorName = (result[0] !== undefined) ? supervisorNamePull(item.supervisor) : null;
+    });
+    this.setState({parts: filteredArray});
+  });
+  }
   render () {
     return (
       <div>
-      <StaffFetch />
+      <StaffFetch parts={this.state.parts}/>
     </div>
     );
   }
