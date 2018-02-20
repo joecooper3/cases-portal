75f8c810d42b7cf195c8d566ef81007afa242adb
Joe Cooper <joecooper3@gmail.com>
2018-02-20 10:57:22 -0500

    Fixes some styles, front page makeshift New Hires

--- template-parts/content-newstaff.php
+++ template-parts/content-newstaff.php
@@ -21,9 +21,10 @@ while ($query->have_posts()) :
 <?php
 if ( is_front_page()): ?>
 <div class="staff-container">
+  <a href="<?php the_permalink(); ?>">
 <div class="portrait" style="background-image:url('<?php the_post_thumbnail_url(); ?>')"></div>
-<h2 class="name"><?php the_title(); ?></h2>
-<h3 class="meta">Program Assistant, Nathaniel ACT<br/>
+<h2 class="name"><?php the_title(); ?></h2></a>
+<h3 class="meta">
 Start Date: <?php the_field("start_date"); ?></h3>
 <div class="fun-facts">
   <?php the_field("fun_facts"); ?>
