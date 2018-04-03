e985e530c2c032436188aed6f7576e3776ad0b66
Joe Cooper <joecooper3@gmail.com>
2018-04-03 16:37:11 -0400

    Adds the in production string in React in webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -39,6 +39,9 @@ var config = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new ExtractTextPlugin('style.css'),
     new UglifyJSPlugin(),
     new webpack.DefinePlugin({
