2d73d09708c263cf87698ed2ae06e9a1122880d1
Joe Cooper <joecooper3@gmail.com>
2018-05-29 15:29:54 -0400

    Edits functions.php so a users request queries all users

--- functions.php
+++ functions.php
@@ -657,6 +657,24 @@ function add_compliance_privacy_role() {
    }
 add_action(init, 'add_compliance_privacy_role' );
 
+add_filter( 'rest_user_query', 'prefix_remove_has_published_posts_from_wp_api_user_query', 10, 2 );
+/**
+ * Removes `has_published_posts` from the query args so even users who have not
+ * published content are returned by the request.
+ *
+ * @see https://developer.wordpress.org/reference/classes/wp_user_query/
+ *
+ * @param array           $prepared_args Array of arguments for WP_User_Query.
+ * @param WP_REST_Request $request       The current request.
+ *
+ * @return array
+ */
+function prefix_remove_has_published_posts_from_wp_api_user_query( $prepared_args, $request ) {
+	unset( $prepared_args['has_published_posts'] );
+
+	return $prepared_args;
+}
+
 function modify_read_more_link() {
     return '<div class="more-link-container"><a class="more-link" href="' . get_permalink() . '">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
 </a></div>';
