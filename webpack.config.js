f9350af7e14243c8cf6a065d4e484b052d3610c9
Joe Cooper <joecooper3@gmail.com>
2018-06-04 15:30:19 -0400

    Changes webpack config files to only build out master.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,6 @@ const SCSS_DIR = path.resolve(__dirname, 'sass');
 
 const config = {
   entry: {
-    index: `${SRC_DIR}/index.jsx`,
-    department: `${SRC_DIR}/department.jsx`,
-    staff: `${SRC_DIR}/staff.jsx`,
-    program: `${SRC_DIR}/program.jsx`,
-    'search-only': `${SRC_DIR}/search-only.jsx`,
-    directory: `${SRC_DIR}/directory.jsx`,
-    compliance: `${SRC_DIR}/compliance.jsx`,
-    resources: `${SRC_DIR}/resources.jsx`,
-    'department-directory': `${SRC_DIR}/department-directory.jsx`,
-    'comms-archive': `${SRC_DIR}/comms-archive.jsx`,
     master: `${SRC_DIR}/master.jsx`
   },
   output: {
