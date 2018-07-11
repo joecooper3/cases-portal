0eee32f87795ed8b63cd9f8ed29af7cf33ca484e
Joe Cooper <joecooper3@gmail.com>
2018-07-11 17:49:20 -0400

    Adds sliding functionality to new staff entries

--- src/components/NewStaff.jsx
+++ src/components/NewStaff.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { NewStaffBox } from './NewStaffBox.jsx';
+import NewStaffSlide from './NewStaffSlide.jsx';
 import { NewStaffPermissions } from './NewStaffPermissions.jsx';
 
 const STAFF_PER_PAGE = 8;
 const MAX_PAGES = 5;
+
+const newStaffContainer = document.getElementById('new-staff-container');
 
 class NewStaff extends React.Component {
   constructor() {
@@ -13,28 +15,43 @@ class NewStaff extends React.Component {
     this.state = {
       moreStaffVisible: true,
       prevStaffVisible: false,
-      sliceOne: 0,
-      sliceTwo: STAFF_PER_PAGE,
-      currentPage: 1
+      currentPage: 1,
+      slideHeights: ['2500px', '2500px', '2500px', '2500px', '2500px'],
+      height: '2500px'
     };
     this._prevStaff = this._prevStaff.bind(this);
     this._moreStaff = this._moreStaff.bind(this);
     this._statusCheck = this._statusCheck.bind(this);
+    this._setHeight = this._setHeight.bind(this);
   }
-
+  componentDidMount() {
+    const slideArr = newStaffContainer.querySelectorAll('.new-hires-slide');
+    const slideHeightArr = [];
+    slideArr.forEach(item => slideHeightArr.push(getComputedStyle(item).height));
+    const slideHeightAdj = slideHeightArr.map(item => {
+      const raw = parseInt(item.split('px')[0]);
+      const modded = (raw + 86).toString();
+      return `${modded}px`;
+    });
+    console.log(slideHeightAdj);
+    console.log(slideHeightArr);
+    this.setState({ slideHeights: slideHeightAdj }); // eslint-disable-line
+    [newStaffContainer.style.height] = slideHeightAdj;
+  }
   _moreStaff() {
     const newPageNumber = this.state.currentPage + 1;
-    this.setState({ sliceOne: this.state.sliceOne + STAFF_PER_PAGE });
-    this.setState({ sliceTwo: this.state.sliceTwo + STAFF_PER_PAGE });
     this.setState({ currentPage: newPageNumber });
     this._statusCheck(newPageNumber);
+    newStaffContainer.style.height = this.state.slideHeights[newPageNumber - 1];
   }
   _prevStaff() {
     const newPageNumber = this.state.currentPage - 1;
-    this.setState({ sliceOne: this.state.sliceOne - STAFF_PER_PAGE });
-    this.setState({ sliceTwo: this.state.sliceTwo - STAFF_PER_PAGE });
     this.setState({ currentPage: newPageNumber });
     this._statusCheck(newPageNumber);
+    newStaffContainer.style.height = this.state.slideHeights[newPageNumber - 1];
+  }
+  _setHeight() {
+    newStaffContainer.style.height = this.state.height;
   }
   _statusCheck(pageNumber) {
     if (pageNumber < 2 && this.state.prevStaffVisible) {
@@ -48,26 +65,31 @@ class NewStaff extends React.Component {
       this.setState({ moreStaffVisible: true });
     }
   }
-
   render() {
+    const arrSlicer = (orig, inp) => {
+      const currentArr = orig[orig.length - 1] || 1;
+      if (currentArr.length < STAFF_PER_PAGE) {
+        currentArr.push(inp);
+      } else {
+        orig.push([inp]);
+      }
+      return orig;
+    };
+    const preparedData = this.props.parts
+      .slice(0, STAFF_PER_PAGE * MAX_PAGES)
+      .reduce(arrSlicer, []);
     const { prevStaffVisible } = this.state;
     const { moreStaffVisible } = this.state;
     return (
-      <div id="new-hires">
-        {this.props.parts
-          .slice(this.state.sliceOne, this.state.sliceTwo)
-          .map(part => (
-            <NewStaffBox
-              key={part.id}
-              first={part.first}
-              last={part.last}
-              title={part.title}
-              imageUrl={part.imageUrl}
-              url={part.url}
-              startDate={part.startDate}
-              funFacts={part.funFacts}
-            />
-          ))}
+      <React.Fragment>
+        {preparedData.map((inp, i) => {
+          if (i + 1 === this.state.currentPage) {
+            return <NewStaffSlide key={i} data={inp} cssClasses="new-hires-slide active" />;
+          } else if (i < this.state.currentPage) {
+            return <NewStaffSlide key={i} data={inp} cssClasses="new-hires-slide on-left" />;
+          }
+          return <NewStaffSlide key={i} data={inp} cssClasses="new-hires-slide" />;
+        })}
         <div id="new-staff-buttons">
           {prevStaffVisible ? (
             <div onClick={this._prevStaff} onKeyDown={this._prevStaff} role="button" tabIndex="0">
@@ -84,8 +106,11 @@ class NewStaff extends React.Component {
             <br />
           )}
         </div>
+        <div>
+          Page {this.state.currentPage} of {MAX_PAGES}
+        </div>
         {this.props.perm && <NewStaffPermissions />}
-      </div>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/NewStaffSlide.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { NewStaffBox } from './NewStaffBox.jsx';
+
+const NewStaffSlide = ({ cssClasses, data }) => (
+  <div className={cssClasses}>
+    {data.map(part => (
+      <NewStaffBox
+        key={part.id}
+        first={part.first}
+        last={part.last}
+        title={part.title}
+        imageUrl={part.imageUrl}
+        url={part.url}
+        startDate={part.startDate}
+        funFacts={part.funFacts}
+      />
+    ))}
+  </div>
+);
+
+NewStaffSlide.propTypes = {
+  cssClasses: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired
+};
+
+export default NewStaffSlide;
